3111f2794e09a338895aa2b1649ded0070e830e3
Nicholas Manha <nickrmanha@gmail.com>
2022-06-09 14:24:20 -0700

    Security Change

--- blog/settings.py
+++ blog/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tpvtb0u4v_1h1cs#6)#)5a78ev)*#^kfg*innc^s=68g_2yay_'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
